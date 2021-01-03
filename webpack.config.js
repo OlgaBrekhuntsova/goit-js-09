403926164a0745b850f3b400ede37857854a3d5f
OlgaBrekhuntsova <olga.pistryak@gmail.com>
2021-01-03 18:01:35 +0200

    Webpack updated

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,27 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
-    // context: path.resolve(__dirname, "src"),
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "build"),
-        filename: "bundle.js"
-    },
-    module: {
+  context: path.resolve(__dirname, "src"),
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  },
+  module: {
     rules: [
       {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: "babel-loader"
-          // options: {
-          //   presets: ['@babel/preset-env']
-          // }
-          },
+        // options: {
+        //   presets: ['@babel/preset-env']
+        // }
+      },
       {
         test: /\.scss$/i,
         use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
     ]
-  }, 
-  plugins: [new MiniCssExtractPlugin({ filename: 'styles.css' }), new HtmlWebpackPlugin({template: './src/index.html'})]
+  },
+  plugins: [new MiniCssExtractPlugin({ filename: 'styles.css' }), new HtmlWebpackPlugin({ template: './index.html' })]
 };
